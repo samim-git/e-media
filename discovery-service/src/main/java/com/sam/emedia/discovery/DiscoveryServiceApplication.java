17d3fe0ca95ceac83a8b8b4662c4f9b3810b359a
Hafizullah Samim <hafizullahsamim@Hafizullahs-Laptop.local>
2023-01-10 10:48:51 +0100

    application dockerized

--- discovery-service/src/main/java/com/sam/emedia/discovery/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/sam/emedia/discovery/DiscoveryServiceApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+
 @EnableEurekaServer
+@SpringBootApplication
 public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
